3363cb7f3bb5e105bc36b090b802a69d7b1e065c
mromero2024 <mromero@40-MROMERO.ENOD_LARIOJA.COM>
2023-04-03 09:56:30 -0300

    act

--- clase6/src/clase6/inicio.java
+++ clase6/src/clase6/inicio.java
@@ -8,7 +8,7 @@ public class inicio {
 		// TODO Auto-generated method stub
 		
 		calculadora nuevoCalculo = new calculadora();
-		System.out.println("-------ingrese el numero de la operacion que desea realizar Suma: 1 Resta: 2  Multiplicacion: 3 Division: 4");
+		System.out.println("-------ingrese el numero de la operacion que desea realizar Suma: 1 Resta: 2  Multiplicacion: 3 Division: 4: Multiplicacion");
 		Scanner tipoOperacion = new Scanner(System.in);
 		String calcular = tipoOperacion.nextLine();
 		int decision = Integer.parseInt(calcular);
