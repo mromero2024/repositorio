24699d5c2c3474439b63ca20fedf11af3b26e0ef
mromero2024 <mromero@40-MROMERO.ENOD_LARIOJA.COM>
2023-04-14 15:53:57 -0300

    Integrado supuesto de 1 partido y 1 pronostico

--- integrador/src/integrador/rutas.java
+++ integrador/src/integrador/rutas.java
@@ -15,8 +15,6 @@ public class rutas extends resultados{
 		String equipo2 = arreglo[2];
 		int golesE1 = Integer.parseInt(arreglo[1]);
 		int golesE2 = Integer.parseInt(arreglo[3]);
-		System.out.println("Resultados del primer partido:" + "\n");
-		System.out.println(equipo1 + ":" + golesE1 + "\n" + equipo2 + ":" + golesE2);
 		
 		FileReader archivoRuta2 = new FileReader(rPronostico);
 		BufferedReader lectorRuta2 = new BufferedReader(archivoRuta2);
@@ -24,26 +22,30 @@ public class rutas extends resultados{
 		String arreglo2 [] = cadena2.split(",");
 		String pronostico1 = arreglo2[0];
 		String pronostico2 = arreglo2[1];
-		System.out.println("--------------" + "\n");
-		System.out.println("Pronostico1:" + pronostico1 +  "\n" + "Pronostico2:" + pronostico2);
-		
+	
 		String resultado1;
 		if(golesE1 > golesE2) {
 			resultado1= equipo1;
-			System.out.println(resultado1 + "<---");
+			
 		}else if(golesE1 < golesE2){
 			resultado1= equipo2;
-			System.out.println(resultado1 + "<---");
+			
 		}else {
 			resultado1= "EMPATE";
 		}
+		System.out.println("Resultado del primer partido: " + "\n" +  resultado1 + "\n" );
 		
 		int puntaje1 = 0;
 		if(resultado1.equals(pronostico1)) {
 			puntaje1++;
-			System.out.println("Ganaste 1 punto");
+			System.out.println("Tu Pronostico: "+ "\n" + pronostico1 + "\n" + "Ganaste 1 punto" + "\n");
+			System.out.println("-----------------------");
 			System.out.println("Puntaje total: " + puntaje1);
-		}else{System.out.println("no ganaste nada");};
+		}else{
+			System.out.println("Tu Pronostico: "+ "\n" + pronostico1 + "\n" + "Perdiste" + "\n");
+			System.out.println("-----------------------");
+			System.out.println("Puntaje total: " + puntaje1);
+			}
 		
 	}catch(Exception e) {
 		System.out.println("error");
